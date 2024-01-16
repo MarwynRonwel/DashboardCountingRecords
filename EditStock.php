c134e456846d24b0d422304934ea973a6090bc8a
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-01-16 21:19:07 +0800

    Changes: Dynamic select option

--- EditStock.php
+++ EditStock.php
@@ -24,6 +24,9 @@
     $result = $conn->query("SELECT * FROM Products WHERE id = $id");
     $row = $result->fetch_assoc();
 
+    $category = $conn->query("SELECT DISTINCT category FROM Products");
+    $categories = $category->fetch_all(MYSQLI_ASSOC);
+
     echo "<section class='flex justify-center'>
           <form action='./api/edit_stock.php' method='post' class='p-20'>
               <h1 class='text-lg font-bold text-gray-600'>Update: #{$row['id']} item</h1>
@@ -32,20 +35,21 @@
                   <label for='name' class='text-sm font-semibold text-gray-700 my-2'>Product Name</label>
                   <input type='text' name='name' id='name' placeholder='Product name' value='{$row['productName']}' class='block border border-gray-700 py-1.5 pl-1.5 pr-5 rounded-md'>
               </div>
-  
-              <div class='my-5'>
-                  <label for='category' class='text-sm font-semibold text-gray-700 my-2'>Select category</label>
-                  <select name='category' id='category' class='block border border-gray-700 py-1.5 pl-1.5 pr-5 rounded-md'>
-                  <option value='None' " . ($row['category'] == 'None' ? 'selected' : '') . ">None</option>
-                  <option value='Gadget' " . ($row['category'] == 'Gadget' ? 'selected' : '') . ">Gadget</option>
-                  <option value='Dress' " . ($row['category'] == 'Dress' ? 'selected' : '') . ">Dress</option>
-                  <option value='Accessory' " . ($row['category'] == 'Accessory' ? 'selected' : '') . ">Accessory</option>
-                  <option value='Furniture' " . ($row['category'] == 'Furniture' ? 'selected' : '') . ">Furniture</option>
 
-                  </select>
+            <div class='my-5'>
+                  <label for='category' class='text-sm font-semibold text-gray-700 my-2'>Select category</label>
+                  <select name='category' id='category' class='block border border-gray-700 py-1.5 pl-1.5 pr-5 rounded-md'>";
+
+    foreach ($categories as $category) {
+        $categoryValue = $category['category'];
+        $selected = ($row['category'] == $categoryValue) ? 'selected' : '';
+        echo "<option value='$categoryValue' $selected>$categoryValue</option>";
+    }
+
+    echo "</select>
               </div>
-  
-              <div class='my-5'>
+
+    <div class='my-5'>
                   <label for='price' class='text-sm font-semibold text-gray-700 my-2'>Price</label>
                   <input type='number' name='price' id='price' placeholder='Price' class='block border border-gray-700 py-1.5 pl-1.5 pr-5 rounded-md' value='{$row['price']}'>
               </div>
